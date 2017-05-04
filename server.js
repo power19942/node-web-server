1040c2ae0c66eae948b512f7300b81e6c31958ea
omar jakmira <poweromar19942@gmail.com>
2017-05-03 19:43:53 -0800

    enable views

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use((req,res,next)=>{
     console.log(`${now}: ${req.method} / ${req.url}`);
     next();
 });
-app.use((req,res,next)=>{
-   res.render("maintenance.hbs");
-});
+// app.use((req,res,next)=>{
+//    res.render("maintenance.hbs");
+// });
 app.use(express.static(__dirname+'/public'));
 app.get('/',(req,res)=>{
     res.send("hello");
